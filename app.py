00e593e57f0083284dc97872e2204f7ecf9ef43d
jcshe <jcshelly66@gmail.com>
2025-08-07 17:36:15 -0700

    test move to csv.

--- app.py
+++ app.py
@@ -151,6 +151,7 @@ view_mode = st.selectbox("View Mode", ["Desktop", "Mobile"], index=0)
 try:
     df = pd.read_csv(
         FILE_PATH,
+        encoding="utf-8",
         parse_dates=["Checkout Date", "Return Date"]
     )
     df["Checkout Date"] = df["Checkout Date"].dt.normalize()
