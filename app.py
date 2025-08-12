23affdf194487fc113c399cf51530c935e822eab
jcshe <jcshelly66@gmail.com>
2025-08-12 14:48:03 -0700

    Fully working

--- app.py
+++ app.py
@@ -575,8 +575,7 @@ def display_management_interface(df):
                                                                required=True),
                     "Checkout Date": st.column_config.DateColumn("Checkout", required=True),
                     "Return Date": st.column_config.DateColumn("Return", required=True),
-                    "Authorized Drivers": st.column_config.TextColumn("Authorized Drivers")
-                    # Keep as text for simplicity in data_editor
+                    "Authorized Drivers": st.column_config.SelectboxColumn("Authorized Drivers",options=load_lookup_list(DRIVERS_LIST_PATH),required=True)
                 },
                 key="data_editor"
             )
